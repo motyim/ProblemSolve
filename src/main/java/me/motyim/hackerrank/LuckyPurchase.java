8c6176ec844792fd6e80b03047243a5f88052c14
Mohamed MotYim <mohamed.motyim@gmail.com>
2017-11-13 14:53:43 +0200

    solve LuckyPurchase [hacker rank]

---
+++ src/main/java/me/motyim/hackerrank/LuckyPurchase.java
@@ -0,0 +1,57 @@
+package me.motyim.hackerrank;
+
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+import java.util.regex.Pattern;
+
+/**
+ * @author MotYim <mohamed.motyim@gmail.com>
+ * @since 13-Nov-17
+ * link : https://www.hackerrank.com/contests/w35/challenges/lucky-purchase
+ */
+public class LuckyPurchase {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        //make it sorted on key
+        Map<Integer , String> map = new TreeMap <>();
+
+        Pattern pattern = Pattern.compile("[^47]");
+
+        int n = in.nextInt();
+        for(int a0 = 0; a0 < n; a0++){
+            String s = in.next();
+            int m = in.nextInt();
+            String check = ""+m ;
+
+            //must have even length
+            if (check.length() % 2 != 0 ) continue;
+
+            //check if contain other numbers
+            if(pattern.matcher(check).find()) continue;
+
+            //check equality of 7 & 4 digit
+            if(!checkEqulity(check)) continue;
+
+            map.put(m , s);
+        }
+
+        if(map.size() == 0 ) System.out.println(-1);
+        else{
+            int firstKey = map.keySet().iterator().next();
+            System.out.println(map.get(firstKey ));
+        }
+
+        in.close();
+
+
+    }
+
+    private static boolean checkEqulity(String check) {
+        return check.length() / 2 == check.replaceAll("7", "").length();
+    }
+
+}
---
+++ src/test/java/me/motyim/hackerrank/LuckyPurchaseTest.java
@@ -0,0 +1,87 @@
+package me.motyim.hackerrank;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
+/**
+ * @author MotYim <mohamed.motyim@gmail.com>
+ * @since 13-Nov-17
+ */
+public class LuckyPurchaseTest {
+
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
+
+
+    @Before
+    public void setUpStreams() {
+        // After this all System.out.println() statements will come to outContent stream.
+        System.setOut(new PrintStream(outContent));
+        // After this all System.err.print() statements will come to errContent stream.
+        System.setErr(new PrintStream(errContent));
+
+    }
+
+    @Test
+    public void testCase1(){
+
+        String testInput = "4\n" +
+                "HackerBook 777444\n" +
+                "RankBook 3\n" +
+                "TheBook 777\n" +
+                "BestBook 47";
+
+        String expectedOutput  = "BestBook";
+
+        // After this he get input form inContent
+        ByteArrayInputStream inContent  = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(inContent);
+
+
+        //call main method of class
+        LuckyPurchase.main(null);
+
+        //trim to handle extra space
+        String output = outContent.toString().trim();
+        assertThat(output, is(expectedOutput));
+        System.setIn(System.in);
+    }
+
+    @Test
+    public void testCase2(){
+
+        String testInput = "1\n" +
+                "abacab 121";
+
+        String expectedOutput  = "-1";
+
+        // After this he get input form inContent
+        ByteArrayInputStream inContent  = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(inContent);
+
+
+        //call main method of class
+        LuckyPurchase.main(null);
+
+        //trim to handle extra space
+        String output = outContent.toString().trim();
+        assertThat(output, is(expectedOutput));
+        System.setIn(System.in);
+    }
+
+    @After
+    public void cleanUpStreams() {
+        //reset after end
+        System.setOut(System.out);
+        System.setErr(System.err);
+    }
+
+}
