1ebf089d30324c0558b89e1abca6c10080543ac3
Mohamed MotYim <mohamed.motyim@gmail.com>
2017-11-15 11:04:56 +0200

    solve triple recursion [hacker renk]

--- src/main/java/me/motyim/hackerrank/TripleRecursion.java
+++ src/main/java/me/motyim/hackerrank/TripleRecursion.java
@@ -1,8 +1,54 @@
 package me.motyim.hackerrank;
 
+import java.util.Scanner;
+
+
+
 /**
  * @author MotYim <mohamed.motyim@gmail.com>
  * @since 15-Nov-17
  */
 public class TripleRecursion {
+
+    static void tripleRecursion(int n, int m, int k) {
+
+        int [][] a = new int [n][n];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                if( i == 0 && j == 0) a[i][j] = m;
+
+                else if (i == j) a[i][j] = a[i-1][j-1] + k;
+
+                else if(i > j ) a[i][j] = a[i-1][j]-1;
+
+                else a[i][j] = a[i][j-1] -1 ;
+
+            }
+        }
+
+        printArray(a);
+
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int m = in.nextInt();
+        int k = in.nextInt();
+        tripleRecursion(n, m, k);
+        in.close();
+    }
+
+    public static void printArray(int [] [] a ){
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[i].length; j++) {
+                System.out.printf("%d ",a[i][j]);
+            }
+            System.out.println();
+        }
+    }
+
+
 }
--- src/test/java/me/motyim/hackerrank/TripleRecursionTest.java
+++ src/test/java/me/motyim/hackerrank/TripleRecursionTest.java
@@ -1,9 +1,64 @@
-import static org.junit.Assert.*;
+package me.motyim.hackerrank;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
 
 /**
  * @author MotYim <mohamed.motyim@gmail.com>
  * @since 15-Nov-17
  */
 public class TripleRecursionTest {
+    private final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+    private final ByteArrayOutputStream errContent = new ByteArrayOutputStream();
 
+
+    @Before
+    public void setUpStreams() {
+        // After this all System.out.println() statements will come to outContent stream.
+        System.setOut(new PrintStream(outContent));
+        // After this all System.err.print() statements will come to errContent stream.
+        System.setErr(new PrintStream(errContent));
+
+    }
+
+    @Test
+    public void testCase1(){
+
+        String testInput = "5 10 7";
+
+        String expectedOutput  = "10 9 8 7 6\n" +
+                "9 17 16 15 14\n" +
+                "8 16 24 23 22\n" +
+                "7 15 23 31 30\n" +
+                "6 14 22 30 38";
+
+        // After this he get input form inContent
+        ByteArrayInputStream inContent  = new ByteArrayInputStream(testInput.getBytes());
+        System.setIn(inContent);
+
+
+        //call main method of class
+        TripleRecursion.main(null);
+
+        //trim to handle extra space
+        String output = outContent.toString().trim();
+        assertThat(output, is(expectedOutput));
+        System.setIn(System.in);
+    }
+
+
+    @After
+    public void cleanUpStreams() {
+        //reset after end
+        System.setOut(System.out);
+        System.setErr(System.err);
+    }
 }
