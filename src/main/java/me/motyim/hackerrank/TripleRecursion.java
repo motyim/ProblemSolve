f12bcb36d481d91790d05ebb927f52dec82d4c43
Mohamed MotYim <mohamed.motyim@gmail.com>
2017-11-15 11:04:28 +0200

    solve Triple Recursion [hacker rank]

---
+++ src/main/java/me/motyim/hackerrank/TripleRecursion.java
@@ -0,0 +1,8 @@
+package me.motyim.hackerrank;
+
+/**
+ * @author MotYim <mohamed.motyim@gmail.com>
+ * @since 15-Nov-17
+ */
+public class TripleRecursion {
+}
---
+++ src/test/java/me/motyim/hackerrank/TripleRecursionTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+
+/**
+ * @author MotYim <mohamed.motyim@gmail.com>
+ * @since 15-Nov-17
+ */
+public class TripleRecursionTest {
+
+}
